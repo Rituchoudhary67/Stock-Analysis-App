6f299457b41e0be5cdd3e4dfb6af3e5d61eb2f1c
Ritu Choudhary <114747083+Rituchoudhary67@users.noreply.github.com>
2026-02-12 16:24:28 +0530

    home section added

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 function Pricing() {
     return ( 
-        <h1>pricing</h1>
+       <div className='container'>
+        <div className='row'>
+            <div className='col-4'>
+                <h1 className='mb-3'>Unbeatable pricing</h1>
+                <p>we pianeered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                <a href='' style={{textDecoration:"none"}}>See Pricing <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+            </div>
+            <div className='col-2'></div>
+            <div className='col-6'>
+                <div className='row text-center'>
+                    <div className='col p-4 border'>
+                        <h1 className='mb-3'>₹0</h1>
+                        <p>Free equity delivery and <br/> direct mutual funds</p>
+                    </div>
+                     <div className='col p-4 border'>
+                        <h1 className='mb-3'>₹20</h1>
+                        <p>Intraday anf F&O</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+       </div>
      );
 }
 
--- src/landing_page/home/Stats.js
+++ src/landing_page/home/Stats.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
 function Stats() {
-    return (  
-        <h1>Stats</h1>
+    return (
+      <div className="container p-3">
+        <div className="row p-5">
+          <div className="col-6 p-5">
+            <h1 className='fs-2 mb-5'>Trust with confidence</h1>
+            <h2 className='fs-4'>Customer-first always</h2>
+            <p className='text-muted'>
+              That's why 1.3+ core customers trust ous with 3.6+ lakh coreres
+              worth of equity investments.
+            </p>
+            <h2 className='fs-4'>Customer-first always</h2>
+            <p className='text-muted'>
+              That's why 1.3+ core customers trust ous with 3.6+ lakh coreres
+              worth of equity investments.
+            </p>
+            <h2 className='fs-4'>Customer-first always</h2>
+            <p className='text-muted'>
+              That's why 1.3+ core customers trust ous with 3.6+ lakh coreres
+              worth of equity investments.
+            </p>
+            <h2 className='fs-4'>Customer-first always</h2>
+            <p className='text-muted'>
+              That's why 1.3+ core customers trust ous with 3.6+ lakh coreres
+              worth of equity investments.
+            </p>
+          </div>
+          <div className='col-6 p-5'>
+            <img src='media/images/ecosystem.png' style={{width:"90%"}}/>
+            <div className='text-center p-5'>
+                <a href='' className='mx-5' style={{textDecoration:"none"}}>Explore our products <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                <a href='' style={{textDecoration:"none"}}>Try Kit demo <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
     );
 }
 
